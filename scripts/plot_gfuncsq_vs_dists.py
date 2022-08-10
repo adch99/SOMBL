9954c3475f51bc85c60422f00ca8c57f1e1c5701
Aditya Chincholi <adityachincholi@gmail.com>
2022-08-10 11:52:08 +0530

    Writing a script to plot gfuncsq vs dist. Added parallelization to hamiltonian generation.

--- scripts/plot_gfuncsq_vs_dists.py
+++ scripts/plot_gfuncsq_vs_dists.py
@@ -5,14 +5,15 @@ params = {
     "coupling_const":   0.0,
     "hop_strength":     1.0,
     "disorder":         10,
-    "size":             10,
+    "size":             40,
     "num_runs":         100,
     "nospin":           True
 }
 
 def getData(params):
     filename = "data/" + getFilename(params) + ".dat"
-    
+    data = np.loadtxt(filename)
+    return data.T
 
 def getFilename(params):
     basename = "mbl_nospin" if params["nospin"] else "mbl"
@@ -23,11 +24,32 @@ def getFilename(params):
     basename += f"_N{params['num_runs']}"
     return basename
 
+def fitData(data, params):
+    return None
 
-def plotData(data, params, fitData):
+def plotData(data, params, fit):
     fig, ax = plt.subplots()
     # Scatter plot the data
+    dists = data[0, :]
+    loggfuncsq = np.log(data[1, :])
+    ax.plot(dists, gfuncsq, marker="o", label="sim")
+    ax.set_xlabel(r"$|r_i - r_j|$")
+    ax.set_ylabel(r"$ln\left[G^2(|r_i - r_j|)\right]$")
+    ax.legend()
 
     # Plot the fit
 
     # Print the residual
+
+    filename = "plots/" + getFilename(params)
+    fig.savefig(filename + ".pdf")
+    fig.savefig(filename + ".png")
+
+def main():
+    data = getData(params)
+    fit = fitData(data, params)
+    plotData(data, params, fit)
+
+if __name__ == "__main__":
+    main()
+    plt.show()
