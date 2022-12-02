63eecdfd350881b8fbc612b3c3eb6550af6ffc37
Aditya Chincholi <adityachincholi@gmail.com>
2022-12-02 17:16:23 +0530

    Changed the pbs file generator and runner for batches.

--- scripts/gen_pbs.sh
+++ scripts/gen_pbs.sh
@@ -6,8 +6,10 @@
 # Script to generate a PBS file for input
 # in the cluster
 
-size=40
-runs=100
+size=100
+runs=90
+batchsize=15
+numbatches=6
 hopup=1.0
 hopdn=1.0
 
@@ -21,16 +23,18 @@ coup_step=0.1
 
 couplings=$(seq $coup_low $coup_step $coup_high)
 disorders=$(seq $dis_high -$dis_step $dis_low)
+batchnums=$(seq 1 1 $numbatches)
 
 for coupling in $couplings; do
     for disorder in $disorders; do
-        jobname="mbl_${size}x${size}_W${disorder}_C${coupling}_TU${hopup}_TD${hopdn}_N${runs}"
-        pbs_filename="jobs/${jobname}.pbs"
-        log_filename="logs/${jobname}.log"
-        execname="${jobname}.out"
-        cp build/exact_diag_simulation $execname
-        cat <<END_OF_PROGRAM > ${pbs_filename}
-#!/bin/sh
+        for batchnum in $batchnums; do
+            jobname="mbl_${size}x${size}_W${disorder}_C${coupling}_TU${hopup}_TD${hopdn}_N${runs}_BS${batchsize}_B${batchnum}"
+            pbs_filename="jobs/${jobname}.pbs"
+            log_filename="logs/${jobname}.log"
+            execname="${jobname}.out"
+            # cp build/exact_diag_batch $execname
+            cat <<END_OF_PROGRAM > ${pbs_filename}
+#!/bin/bash
 # The following line specifies the maximum cpu utilization
 # in terms of cpu time (3 days)
 #PBS -l cput=72:00:00
@@ -39,6 +43,7 @@ for coupling in $couplings; do
 #PBS -l walltime=72:00:00
 # The following line merges the stdout and stderr together
 #PBS -j oe
+#PBS -o ${log_filename}
 #PBS -l mem=8gb
 # The following line specifies the name of the job
 #PBS -N ${jobname}
@@ -46,16 +51,19 @@ for coupling in $couplings; do
 cd \${PBS_O_WORKDIR}
 #########################################################################
 
-echo \`hostname\` \`date\` \`pwd\` >> ./trial\$job
-(time ${execname} -n $runs -c $coupling -w $disorder -s $size -u $hopup -d $hopdn) >> ./trial\$job
-echo \`date\` >> ./trial\$job
+hostname
+date
+pwd
+time ${execname} -n $runs --batchsize $batchsize --batch $batchnum -c $coupling -w $disorder -s $size -u $hopup -d $hopdn
+date
 
 cd \${PBS_O_WORKDIR}
-date >> ./trial\$job
-echo Job ended at \`date\` >> ./trial\$job
+date
+echo Job ended at \`date\`
+rm ${execname}
 exit 0
 
 END_OF_PROGRAM
-
+        done
     done
 done
--- scripts/run_jobs.sh
+++ scripts/run_jobs.sh
@@ -2,6 +2,8 @@
 
 size=40
 runs=100
+batchsize=15
+numbatches=6
 hopup=1.0
 hopdn=1.0
 
@@ -15,47 +17,13 @@ coup_step=0.1
 
 couplings=$(seq $coup_low $coup_step $coup_high)
 disorders=$(seq $dis_high -$dis_step $dis_low)
+batchnums=$(seq 1 1 $numbatches)
 
-chunksize=20
-numchunks=$((runs / chunksize))
-if [ "$((runs % chunksize))" != 0 ]; then
-    echo "We need the number of runs to be a multiple of the chunksize!"
-    echo "$runs is not a multiple of $chunksize"
-    exit 1
-fi
-
-
-
-# nproc=4
-# procnum=1
 for coupling in $couplings; do
     for disorder in $disorders; do
-        jobfilename="jobs/mbl_${size}x${size}_W${disorder}_C${coupling}_TU${hopup}_TD${hopdn}_N${runs}.pbs"
-        qsub $jobfilename
-        # ./build/exact_diag_simulation -n $runs -c $coupling -w $disorder -s $size -u $hopup -d $hopdn &
-        # rem=$((procnum % nproc))
-        # if [ "$rem" = "0" ]; then
-        #     wait
-        # fi
-        # procnum=$((procnum+1))
+        for batchnum in $batchnums; do
+            jobfilename="jobs/mbl_${size}x${size}_W${disorder}_C${coupling}_TU${hopup}_TD${hopdn}_N${runs}_BS${batchsize}_B${batchnum}.pbs"
+            qsub $jobfilename
+        done
     done
 done
-wait
-
-
-function run_chunked
-# {
-#     # $1 = number of runs
-#     # $2 = chunk size
-#     # $3 = coupling strength
-#     # $4 = disorder
-#     numchunks=$(($1 / $2))
-#     if [ "$(($1 % $2))" != 0 ]; then
-#         echo "We need the chunksize to be a multiple of number of runs!"
-#         echo "$1 is not a multiple of $2"
-#         exit 1
-#     fi
-#     for chunkidx in $(seq 1 $numchunks); do
-#         simple "./build/exact_diag_chunk -k $chunkidx -n $2 -c $3 -w $4 -s $size -u $hopup -d $hopdn" 5 &
-#     done
-# }
