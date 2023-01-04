939942067df55f96e4e1f3265aec7e51d8de11b9
Aditya Chincholi <adityachincholi@gmail.com>
2023-01-04 16:03:30 +0530

    Modified pbs generator and runner.

--- scripts/gen_pbs_averaging.sh
+++ scripts/gen_pbs_averaging.sh
@@ -16,12 +16,12 @@ bins=50
 hopup=1.0
 hopdn=1.0
 
-dis_high=18
-dis_low=8.0
+dis_high=12
+dis_low=10
 dis_step=1.0
 
-coup_high=2.0
-coup_low=0.0
+coup_high=1.7
+coup_low=1.5
 coup_step=0.1
 
 couplings=$(seq $coup_low $coup_step $coup_high)
@@ -30,11 +30,14 @@ batchnums=$(seq 1 1 $numbatches)
 
 for coupling in $couplings; do
     for disorder in $disorders; do
-        jobname="mbl_average_${size}x${size}_W${disorder}_C${coupling}_TU${hopup}_TD${hopdn}_N${runs}_BS${batchsize}"
+        # jobname="mbl_average_${size}x${size}_W${disorder}_C${coupling}_TU${hopup}_TD${hopdn}_N${runs}_BS${batchsize}"
+        jobname="mbl_density_${size}x${size}_W${disorder}_C${coupling}_TU${hopup}_TD${hopdn}_N${runs}"
         pbs_filename="jobs/${jobname}.pbs"
         log_filename="logs/${jobname}.log"
         # execname="build/batch_average"
-        execname="build/keldysh_energy_batch_average"
+        # execname="build/keldysh_energy_batch_average"
+        execname="build/keldysh_densities"
+        echo "Out: $pbs_filename"
         cat <<END_OF_PROGRAM > ${pbs_filename}
 #!/bin/bash
 # The following line specifies the maximum cpu utilization
--- scripts/run_jobs_parted.py
+++ scripts/run_jobs_parted.py
@@ -8,12 +8,12 @@ hopup = 1.0
 hopdn = 1.0
 
 
-coupLow = 0.3
-coupHigh = 0.5
+coupLow = 1.5
+coupHigh = 1.7
 coupStep = 0.1
 
-disLow = 13
-disHigh = 15
+disLow = 10
+disHigh = 12
 disStep = 1
 
 numCoups = 3
@@ -23,7 +23,7 @@ numRuns = numCoups * numDis
 start = 0
 stop = 9
 
-basename = "mbl_average"
+basename = "mbl_density"
 
 for index in range(start, stop):
     coupIdx = index // numDis
@@ -31,11 +31,18 @@ for index in range(start, stop):
     coupling = coupLow + coupStep*coupIdx
     disorder = disLow + disStep*disIdx
 
-    for batchnum in range(1, numbatches+1):
-        subs = (basename, size, size,
-                disorder, coupling,
-                hopup, hopdn, runs,
-                batchsize, batchnum)
-        jobfilename = "jobs/%s_%dx%d_W%.1f_C%.1f_TU%.1f_TD%.1f_N%d_BS%d_B%d.pbs" % subs
-        os.system("echo %s" % jobfilename)
-        # os.system("qsub %s" % jobfilename)
+    # for batchnum in range(1, numbatches+1):
+    #     subs = (basename, size, size,
+    #             disorder, coupling,
+    #             hopup, hopdn, runs,
+    #             batchsize, batchnum)
+    #     jobfilename = "jobs/%s_%dx%d_W%.1f_C%.1f_TU%.1f_TD%.1f_N%d_BS%d_B%d.pbs" % subs
+    #     os.system("echo %s" % jobfilename)
+    #     # os.system("qsub %s" % jobfilename)
+
+    subs = (basename, size, size,
+            disorder, coupling,
+            hopup, hopdn, runs)
+    jobfilename = "jobs/%s_%dx%d_W%.1f_C%.1f_TU%.1f_TD%.1f_N%d.pbs" % subs
+    os.system("echo %s" % jobfilename)
+    # os.system("qsub %s" % jobfilename)
